2dd2537f482214be9811ec59e276cb98684e3643
Matthew Cech <reveriewisp@gmail.com>
2025-04-02 21:26:40 -0700

    Update config.js to use generic name

--- Client/config.js
+++ Client/config.js
@@ -1,5 +1,5 @@
 var config = {};
-config.machineKey = "Solberry"; // The key used to store your machine in the DB
+config.machineKey = "MyName"; // The key used to store your machine in the DB
 config.refreshRateMS = 5000; // Time between reports in milliseconds
 config.target = "http://127.0.0.1:1234/report"; // Target, can be local or remote. Doesn't matter.
 
